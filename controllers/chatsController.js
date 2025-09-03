6e78ee2e6cb80eec76018960727e731b76af1d47
Shiban Ashiq C <80801260+shibanashiqc@users.noreply.github.com>
2025-09-03 14:01:47 +0530

    first commit

---
+++ controllers/chatsController.js
@@ -0,0 +1,86 @@
+import { getSession, getChatList, isExists, sendMessage, formatPhone } from './../whatsapp.js'
+import response from './../response.js'
+
+const getList = (req, res) => {
+    return response(res, 200, true, '', getChatList(res.locals.sessionId))
+}
+
+const send = async (req, res) => {
+    const session = getSession(res.locals.sessionId)
+    const receiver = formatPhone(req.body.receiver)
+    const delay = req.body.delay
+    const { message } = req.body
+
+    try {
+        const exists = await isExists(session, receiver)
+
+        if (!exists) {
+            return response(res, 400, false, 'The receiver number is not exists.')
+        }
+        
+        var receive = await sendMessage(session, receiver, message, delay)
+        const messageType = Object.keys (receive.message)[0];
+        var messageId = receive.key.id;
+        
+        //console.log(receive, messageType, messageId)
+
+        response(res, 200, true, 'The message has been successfully sent.', {
+            receive,
+            messageType,
+            messageId
+        })
+    } catch (e) {
+        response(res, 500, false, 'Failed to send the messages. ' + e.message)
+    }
+}
+
+const sendBulk = async (req, res) => {
+    const session = getSession(res.locals.sessionId)
+    const errors = []
+
+    for (const [key, data] of req.body.entries()) {
+        let { receiver, message, delay } = data
+
+        if (!receiver || !message) {
+            errors.push(key)
+
+            continue
+        }
+
+        if (!delay || isNaN(delay)) {
+            delay = 1000
+        }
+
+        receiver = formatPhone(receiver)
+
+        try {
+            const exists = await isExists(session, receiver)
+
+            if (!exists) {
+                errors.push(key)
+
+                continue
+            }
+
+            await sendMessage(session, receiver, message, delay)
+        } catch {
+            errors.push(key)
+        }
+    }
+
+    if (errors.length === 0) {
+        return response(res, 200, true, 'All messages has been successfully sent.')
+    }
+
+    const isAllFailed = errors.length === req.body.length
+
+    response(
+        res,
+        isAllFailed ? 500 : 200,
+        !isAllFailed,
+        isAllFailed ? 'Failed to send all messages.' : 'Some messages has been successfully sent.',
+        { errors }
+    )
+}
+
+export { getList, send, sendBulk }
